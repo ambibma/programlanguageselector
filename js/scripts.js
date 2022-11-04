ac1dd5ac132a5db5c0cddd14e5fe402d7f9d130c
ambi hidalgo <ambio.pk@gmail.com>
2022-11-03 19:26:55 -0700

    Add Questions that will handle for branching, add submit button, add handleSubmit event.

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,21 @@
 window.addEventListener("load" , function() {
   const startQuizBtn = document.getElementById("start-quiz");
   let quizStartText = document.getElementById("quiz-start-text");
-  let quizPage1Text = docuemt.getElementById("quiz-page1-text")
-  let quizPage1Form = document.getElementById("form-div");
-  let quizPage2Form = document.getElementById("question2-div");
-  const nextPageBtn = document.getElementById("page2");
-
+  let quizPage1Form = document.getElementById("quiz-page1");
+  let submitBttn = document.getElementById("submit");
 function toQuizPage1(){
   quizStartText.classList.add("hidden");
-  quizPage2Form.classList.add("hidden");
-  quizPage1text.classList.remove("hidden");
+  quizPage1Form.classList.remove("hidden");
 
 }
-function toQuizPage2(){
-  quizStartText.classList.add("hidden");
-  quizPage1Form.classList.add("hidden");
-  quizPage2Form.classList.remove("hidden");
-
+function handleSubmit(event){
+  event.preventDefault();
+  const question1Input = document.getElementById("form-question1").value;
+  const question2Input = document.getElementById("question2").value;
+  const question3Input = docuemnt.querySelector("input[name='minecraft']:checked").value;
 }
 
 
-nextPageBtn.addEventListener("click", toQuizPage2);
 startQuizBtn.addEventListener("click" , toQuizPage1);
+submitBttn.addEventListener("submit" , handleSubmit);
 })
