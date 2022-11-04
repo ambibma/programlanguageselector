39ea0f27fe70454269094db413e53a2abb1805a8
ambi hidalgo <ambio.pk@gmail.com>
2022-11-03 18:25:00 -0700

    FAILED Add next button to display other contents of quiz

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,25 @@
 window.addEventListener("load" , function() {
   const startQuizBtn = document.getElementById("start-quiz");
   let quizStartText = document.getElementById("quiz-start-text");
+  let quizPage1Text = docuemt.getElementById("quiz-page1-text")
   let quizPage1Form = document.getElementById("form-div");
+  let quizPage2Form = document.getElementById("question2-div");
+  const nextPageBtn = document.getElementById("page2");
 
 function toQuizPage1(){
-  quizStartText.classList.add("hidden")
-  quizPage1Form.classList.remove("hidden")
+  quizStartText.classList.add("hidden");
+  quizPage2Form.classList.add("hidden");
+  quizPage1text.classList.remove("hidden");
 
 }
+function toQuizPage2(){
+  quizStartText.classList.add("hidden");
+  quizPage1Form.classList.add("hidden");
+  quizPage2Form.classList.remove("hidden");
+
+}
+
+
+nextPageBtn.addEventListener("click", toQuizPage2);
 startQuizBtn.addEventListener("click" , toQuizPage1);
 })
